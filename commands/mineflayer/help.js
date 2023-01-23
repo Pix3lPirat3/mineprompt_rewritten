2d7d0bac2a5262215d251eefac1089374373589c
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2023-01-22 22:22:05 -0800

    Sort alphabetically

--- commands/mineflayer/help.js
+++ commands/mineflayer/help.js
@@ -19,24 +19,24 @@ module.exports = {
       return sender.reply(
         `
 Command ${command.command}
+Usage: ${command.usage || '~'}
 Version: ${command.version || '~'}
 Author: ${command.author || '~'}
 Repository: ${command.repository || '~'}
 Aliases: ${command.aliases || '~'}
 Description: ${command.description || 'There is no description for this command..'}
-Usage: ${command.usage || '~'}
 
 `)
     }
 
     // System/Console Commands, Player Commands
 
-    let system_commands = commander.commands_array.filter(cmd => !cmd.requiresEntity).map(cmd => ({ command: cmd.command, description: cmd.description || 'This command does not have a description..' })).sort((a, b) => a.command > b.command)
+    let system_commands = commander.commands_array.filter(cmd => !cmd.requiresEntity).map(cmd => ({ command: cmd.command, usage: cmd.usage, description: cmd.description || 'This command does not have a description..' })).sort((a, b) => a.command.localeCompare(b.command))
     let system_commands_string = `\nSystem Commands:\n` + stringTable.create(system_commands) + '\n';
     if(sender.type === 'discord') system_commands_string = '```' + system_commands_string + '```';
     sender.reply(system_commands_string)
 
-    let player_commands = commander.commands_array.filter(cmd => cmd.requiresEntity).map(cmd => ({ command: cmd.command, description: cmd.description || 'This command does not have a description..' })).sort((a, b) => a.command > b.command)
+    let player_commands = commander.commands_array.filter(cmd => cmd.requiresEntity).map(cmd => ({ command: cmd.command, usage: cmd.usage, description: cmd.description || 'This command does not have a description..' })).sort((a, b) => a.command.localeCompare(b.command))
     let player_commands_string = `\nPlayer Commands:\n` + stringTable.create(player_commands) + '\n'
     if(sender.type === 'discord') player_commands_string = '```' + player_commands_string + '```';
     sender.reply(player_commands_string)
