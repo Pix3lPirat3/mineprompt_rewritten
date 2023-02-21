ebbd94120da728df2ca2b7d43578f25cfd2a99a1
Jovan-04 <evanne.scherrer@gmail.com>
2023-02-20 21:09:40 -0600

    fix spam.js lol

--- commands/mineflayer/chat/spam.js
+++ commands/mineflayer/chat/spam.js
@@ -1,6 +1,6 @@
 module.exports = {
   command: 'spam',
-  usage: 'spam <stop || delay> <message>',
+  usage: 'spam list || start <message> || stop [id {all}]',
   aliases: ['repeat'],
   description: 'Send a message or a command to the server.',
   requires: {
@@ -8,8 +8,12 @@ module.exports = {
   },
   reload: {
     pre: function() {
-      if(this.timer) console.log(`[Reload] [Spam] Unloading the spam timer (${this.timer})`)
-      clearInterval(this.timer);
+      if(this.timers) console.log(`[Reload] [Spam] Unloading the spam timers (${this.timers})`)
+      for (const timer of this.timers) {
+        console.log(timer)
+        clearInterval(timer.timer)
+      }
+      this.timers = []
     },
     post: function() {
 
@@ -17,19 +21,58 @@ module.exports = {
   },
   author: 'Pix3lPirat3',
   execute: function(sender, command, args) {
-    //todo:Stop on disconnect
-    if(args[0] === "stop") return clearInterval(this.reload.timer);
-    if (!args.length) return sender.reply(`[Send] You must specify a message to send.`);
-    let msg = args.join(' ');
-    if (msg.length > 256) return sender.reply(`[Send] Your message was bigger than 256 characters.`);
+    if (!args.length) return sender.reply(`[${this.command}] ${this.usage}`)
 
-    bot.chat(msg);
-    let timer = setInterval(function() {
-      if(!bot?.entity) return clearInterval(timer);
-      bot.chat(msg);
-    }, 2000)
-    this.reload.timer = timer;
-    console.log('Created Timer:', this.reload.timer);
+    if (!this.reload.timers) {
+      this.reload.timers = []
+    }
 
+    if (args[0] === 'list') {
+      for (const timer of this.reload.timers) {
+        console.log(`[Timer] ID: ${timer.timer} Delay: ${timer.delay} Message: ${timer.message}`)
+      }
+    }
+
+    if (args[0] === 'start') {
+      const delay = parseInt(args[1])
+      const message = args.slice(2).join(' ')
+      if (message.length > 256) return sender.reply(`[Send] Your message was bigger than 256 characters.`)
+      const timer = startTimer(delay, message)
+      this.reload.timers.push(timer)
+      console.log(`[Timer] Created new timer with ID ${timer.timer}`)
+    }
+
+    if (args[0] === 'stop') {
+      if (!args[1]) {
+        for (const timer of this.reload.timers) {
+          clearInterval(timer.timer)
+        }
+        console.log('[Timer] Cleared all timers')
+      } else {
+        for (const i in this.reload.timers) {
+          if ((this.reload.timers[i].timer.toString()) == args[1]) {
+            clearInterval(this.reload.timers[i].timer)
+            this.reload.timers.splice(i, 1)
+            console.log(`[Timer] Cleared timer with ID ${args[1]}`)
+            return
+          }
+        }
+        console.log(`[Timer] No timer found with ID ${args[1]}`)
+        return
+      }
+    }
+
+    function startTimer (delay, message) {
+      const timer = setInterval(function() {
+        bot.chat(message)
+      }, delay)
+
+      const data = {
+        timer: timer,
+        delay: delay,
+        message: message
+      }
+      return data
+    }
   }
 }
