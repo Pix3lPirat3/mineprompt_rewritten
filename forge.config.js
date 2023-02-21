20e028a384cd5859e9d2d44b12e6f64261b85bc0
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2023-02-21 15:26:39 -0800

    Patch Cache - Crash on Release

--- forge.config.js
+++ forge.config.js
@@ -1,18 +1,16 @@
 module.exports = {
   packagerConfig: {
     icon: "src/icons/win/computer",
-    ignore: ["/cache/*", "/mineprompt", "/generate-embed.js", ".tmp", ".todo", "/commands/mineflayer/customs"],
+    ignore: ["^/cache/", "/mineprompt", "/generate-embed.js", ".tmp", ".todo", "/commands/mineflayer/customs"],
   },
   rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        // The ICO file to use as the icon for the generated Setup.exe
-        //setupIcon: '/src/icons/win/computer.ico',
-      }
+  makers: [{
+    name: '@electron-forge/maker-squirrel',
+    config: {
+      // The ICO file to use as the icon for the generated Setup.exe
+      //setupIcon: '/src/icons/win/computer.ico',
     }
-  ],
+  }],
   hooks: {
     prePackage: function(platform, arch) {
 
