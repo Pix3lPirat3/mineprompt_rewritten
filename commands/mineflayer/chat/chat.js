7b3bd2ab9333d630faaa133099c93a19d73ee27e
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2023-03-07 03:01:45 -0800

    Changed send.js to chat.js

---
+++ commands/mineflayer/chat/chat.js
@@ -0,0 +1,19 @@
+module.exports = {
+  command: 'chat',
+  usage: 'chat <message>',
+  aliases: ['send', 'say'],
+  description: 'Send a message or a command to the server.',
+  requires: {
+    entity: true
+  },
+  author: 'Pix3lPirat3',
+  autocomplete: function() {
+    return Object.keys(bot.players);
+  },
+  execute: function(sender, command, args) {
+    if (!args.length) return sender.reply(`[${this.command}] ${this.usage}`);
+    let msg = args.join(' ');
+    if (msg.length > 256) return sender.reply(`[Send] Your message was bigger than 256 characters.`)
+    bot.chat(msg)
+  }
+}
