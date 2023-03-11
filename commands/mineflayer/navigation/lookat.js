4f6d7dfee26e31510fc9c41576182985640237a2
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2023-03-11 04:20:19 -0800

    Planning for future update

--- commands/mineflayer/navigation/lookat.js
+++ commands/mineflayer/navigation/lookat.js
@@ -11,7 +11,7 @@ module.exports = {
   execute: async function(sender, command, args) {
     if (!args.length) return sender.reply(`[${this.command}] ${this.usage}`);
 
-    if(args.length > 0) {
+    if(args.length === 1 || args.length === 2) {
       let force = args[1] || false;
       let block = bot.registry.blocksByName[args[0]];
       if(!block) return sender.reply(`[lookat] There is no block called ${args[0]}`);
@@ -20,6 +20,10 @@ module.exports = {
       sender.reply(`[lookat] Turning towards ${block.name} ${nearest.position}`)
       await bot.lookAt(nearest.position.offset(0.5, 0.5, 0.5), force)
       sender.reply(`[lookat] Now looking at ${block.name} ${nearest.position}`)
+    }
+    if(args.length === 3 || args.length === 4) {
+      let force = args[0] || 'false';
+      // lookat x y z {force}
     }
 
   }
