4f691292b45f09202f7b3a6d893bed5fa9414962
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2023-03-11 04:06:36 -0800

    Added "window" command

---
+++ commands/mineflayer/inventory/window.js
@@ -0,0 +1,60 @@
+let stringTable = require('string-table')
+
+module.exports = {
+  command: 'window',
+  usage: 'window',
+  //aliases: ['inv'],
+  description: 'Show the bot\'s inventory.',
+  requires: {
+    entity: true,
+    console: true
+  },
+  author: 'Pix3lPirat3',
+  execute: function(sender, command, args) {
+    if(!bot.currentWindow) return sender.reply(`[Window] You do not have a window open.`)
+
+    let inventory_items = bot.currentWindow.containerItems();
+    if(!inventory_items.length) return sender.reply(`[window] There are no items in your open window.`);
+
+    let items = Object.values(inventory_items).map((item, index) => ({
+        slot: index,
+        name: item ? item.name : null,
+        count: item ? item.count : null,
+        displayName: item ? getCustomName(item) : null, // displayName gets the outer field (toAnsi breaks character width)
+    }));
+
+    sender.reply(stringTable.create(items))
+
+    function getCustomName(item) {
+      if(!item.customName) return item.displayName;
+      let data = bot.registry.version['<']('1.13') ? item.customName : JSON.parse(item.customName);
+      let ansi = new ChatMessage(data).toAnsi(bot.registry.language, {
+          '§0': '\u001b[38;5;240m',
+          '§1': '\u001b[38;5;19m',
+          '§2': '\u001b[38;5;34m',
+          '§3': '\u001b[38;5;37m',
+          '§4': '\u001b[38;5;124m',
+          '§5': '\u001b[38;5;127m',
+          '§6': '\u001b[38;5;214m',
+          '§7': '\u001b[38;5;250m',
+          '§8': '\u001b[38;5;245m',
+          '§9': '\u001b[38;5;63m',
+          '§a': '\u001b[38;5;83m',
+          '§b': '\u001b[38;5;87m',
+          '§c': '\u001b[38;5;203m',
+          '§d': '\u001b[38;5;207m',
+          '§e': '\u001b[38;5;227m',
+          '§f': '\u001b[97m',
+          '§l': '\u001b[1m',
+          '§o': '\u001b[3m',
+          '§n': '\u001b[4m',
+          '§m': '\u001b[9m',
+          '§k': '\u001b[6m',
+          '§r': '\u001b[0m'
+      });
+      return ansi;
+    }
+
+
+  }
+}
