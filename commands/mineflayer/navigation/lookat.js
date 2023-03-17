3622ff1c64ef5007b73264fe9a8cfdf14d7da364
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2023-03-17 02:05:20 -0700

    Added "lookat xyz"

--- commands/mineflayer/navigation/lookat.js
+++ commands/mineflayer/navigation/lookat.js
@@ -1,8 +1,10 @@
+var v = require('vec3');
+
 // block (findBlock)[0]
 // pos (x, y, z)
 module.exports = {
   command: 'lookat',
-  usage: 'lookat <block|pos> [force {false}]',
+  usage: 'lookat < block | x y z> [force {false}]',
   description: 'Turn to look at a block/position',
   requires: {
     entity: true
@@ -14,16 +16,37 @@ module.exports = {
     if(args.length === 1 || args.length === 2) {
       let force = args[1] || false;
       let block = bot.registry.blocksByName[args[0]];
-      if(!block) return sender.reply(`[lookat] There is no block called ${args[0]}`);
+      if(!block) return sender.reply(`[LookAt] There is no block called ${args[0]}`);
       let nearest = await bot.findBlock({ matching: block.id });
-      if(!nearest) return sender.reply(`[lookat] There are no ${args[0]} blocks nearby.`);
-      sender.reply(`[lookat] Turning towards ${block.name} ${nearest.position}`)
+      if(!nearest) return sender.reply(`[LookAt] There are no ${args[0]} blocks nearby.`);
+      sender.reply(`[LookAt] Turning towards ${block.name} ${nearest.position}`)
       await bot.lookAt(nearest.position.offset(0.5, 0.5, 0.5), force)
-      sender.reply(`[lookat] Now looking at ${block.name} ${nearest.position}`)
+      sender.reply(`[LookAt] Now looking at ${block.name} ${nearest.position}`)
     }
+
+    // lookat x y z {force}
     if(args.length === 3 || args.length === 4) {
-      let force = args[0] || 'false';
-      // lookat x y z {force}
+      let force = args[3] || 'false';
+
+      let [x, y, z] = args;
+      x = cleanInt(x);
+      if (isNaN(x)) return sender.reply(`[LookAt] Unable to parse input, X: ${x} is invalid.`);
+      y = cleanInt(y);
+      if (isNaN(y)) return sender.reply(`[LookAt] Unable to parse input, Y: ${y} is invalid.`);
+      z = cleanInt(z);
+      if (isNaN(y)) return sender.reply(`[LookAt] Unable to parse input, Z: ${z} is invalid.`);
+
+      let block = bot.blockAt(v(x, y, z));
+      sender.reply(`[LookAt] Turning towards ${block.name} ${block.position}`)
+      await bot.lookAt(block.position.offset(0.5, 0.5, 0.5), force)
+      sender.reply(`[LookAt] Now looking at ${block.name} ${block.position}`)
+    }
+
+
+
+    function cleanInt(x) {
+      x = Number(x);
+      return x >= 0 ? Math.floor(x) : Math.ceil(x);
     }
 
   }
