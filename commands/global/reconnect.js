3416bd4c83791704b9f01139861d7f3b1ef2d5ff
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2023-04-20 03:41:22 -0700

    template

---
+++ commands/global/reconnect.js
@@ -0,0 +1,12 @@
+module.exports = {
+  command: 'reconnect',
+  usage: 'reconnect',
+  description: 'Reconnect to the last attempted server.',
+  author: 'Pix3lPirat3',
+  requires: {
+    console: true
+  },
+  execute: async function(sender, command, args) {
+    sender.reply('[Error] This command is not yet implemented.')
+  }
+}
