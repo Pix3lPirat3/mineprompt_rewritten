2cd0c3fbbe6d42e6d1322d2c977354609a99eab7
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2023-06-07 19:58:04 -0700

    Removed remove account button (Use the 'account' command)

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -8,26 +8,13 @@ let sidebar = {
     add: function(account) {
       //console.debug('[sidebar.js] Adding', account)
       $('#sidebar-accounts').append(`<div class="box-user" data-username="${account.username}" data-auth="${account.authentication}" data-tippy-content="${account.username} <ul style='padding-left: 14px;'><li>auth: ${account.authentication}</li></ul>">
-        <div class="account-delete" style="position: relative;">
-        <div class="delete-account" account-delete="${account.username}" style="cursor: pointer; display: none; top: 0; right: 0; position: absolute; color: indianred; border: 2px solid #7e7e7e; border-radius: 25px; padding: 1.5px 3px;">X</div>
         <div class="box-img">
           <img src="https://mc-heads.net/head/${account.username}/nohelm">
-        </div>
         </div>
       </div>`)
     }
   },
   addListeners: function() {
-    $('#sidebar-accounts').on('mouseover', 'div.box-user', function(e) {
-      let elCurrentTarget = $(e.currentTarget); // .box-user
-      elCurrentTarget.find('.delete-account').show()
-    });
-
-    // Delete Account
-    $('#sidebar-accounts').on('mouseout', 'div.box-user', function(e) {
-      let elCurrentTarget = $(e.currentTarget); // .box-user
-      elCurrentTarget.find('.delete-account').hide()
-    });
 
     // Open console for this user--
     $('#sidebar-accounts').on('click', 'div.box-user', function(e) {
@@ -35,9 +22,7 @@ let sidebar = {
       let elClickedElement = $(e.target);
 
       let username = $(el).data('username');
-      let auth = $(el).data('auth');
-
-      if(elClickedElement.attr('account-delete')) return database.removeAccount(username);
+      let auth = $(el).data('auth') == true;
       
       let term_command = `connect -u ${username}`;
       if (auth) term_command += ` -a ${auth}`;
