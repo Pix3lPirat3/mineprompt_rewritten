f5dc96bc835edff403d2b91f960ef8d40b860328
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2023-06-07 19:54:02 -0700

    Changed auth options (auth: "microsoft/offline") to (auth: true/false)
    The options for auth "microsoft/offline" are now simplified to "true/false"

--- commands/global/account.js
+++ commands/global/account.js
@@ -2,7 +2,7 @@ let stringTable = require('string-table')
 
 module.exports = {
   command: 'account',
-  usage: 'account <add/remove> [username] [authentication (microsoft/offline)]',
+  usage: 'account <add/remove> [username] [authentication (true/false)]',
   description: 'Add an account',
   author: 'Pix3lPirat3',
   requires: {
@@ -12,26 +12,30 @@ module.exports = {
 
     if(!args.length) return console.log(`[Account] Invalid Usage: ${this.usage}`);
 
-    let authentication_types = ['microsoft', 'offline'];
-    let authentication = args[2] || 'microsoft';
-    if(!authentication_types.includes(authentication)) {
-      return sender.reply(`[Account] "${authentication}" is not a valid authenticaion type.\nTypes: ${authentication_types.join(', ')}`)
-    }
     let username = args[1];
 
-    if(args[0] === 'add' && args.length >= 2) {
-      const stmt = await database.db.prepare('SELECT * FROM accounts WHERE username = ?;')
-      await stmt.bind({ 1: username })
-      
-      let accounts = await stmt.get();
-      if(accounts) return sender.reply(`[Account] An account with this name already exists..`);
+    if(args[0] === 'add') {
+      if(args.length < 3) return console.log(`[Account] You must specify a [[bu;indianred;]username] and [[bu;indianred;]authentication]`);
+      let authentication = args[2];
+      if(typeof authentication !== 'boolean') return console.log(`[Account] Type of authentication must be [[bu;indianred;]true] or [[bu;indianred;]false]`);
 
+      // Check if the account already exists
+      let accountExists = await database.getAccount(username);
+      if(accountExists) return console.log(`[Account] Unable to create account, account "${username}" already exists.`)
+
+      // Add Account
+      console.log('authentication:', authentication)
       return database.addAccount(username, authentication)
-
     }
 
-    if(args[0] === 'remove' && args.length === 2) {
-        return database.removeAccount(username);
+    if(args[0] === 'remove') {
+      if(args.length === 1) return console.log(`[Account] You must specify an account to remove.`);
+      let accountExists = await database.getAccount(username);
+      if(!accountExists) return console.log(`[Account] Unable to delete account, account "${username}" does not exist.`);
+      console.log(`[Account] Deleted the account "${username}"`)
+      return database.removeAccount(username);
     }
+
+    return console.log(`[${this.command}] ${this.usage}`)
   }
 }
