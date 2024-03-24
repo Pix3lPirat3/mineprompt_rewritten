315f9b8f14ad84878a3ea8b9159af3fcbe1e4571
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2024-03-23 22:34:09 -0700

    Attempting patch for version. auth and general connect issues

--- commands/global/connect.js
+++ commands/global/connect.js
@@ -1,8 +1,8 @@
-const parseArgs = require('minimist');
-let { supportedVersions, testedVersions } = require('mineflayer/lib/version')
+let { parseArgs } = require('node:util');
 
 const path = require('path');
 const minecraftFolderPath = require('minecraft-folder-path');
+
 module.exports = {
   version: '2.0.1',
   description: 'Used to connect a bot to a server',
@@ -27,43 +27,49 @@ module.exports = {
       return console.log(`\n[[;indianred;]Connection Interrupted:]\nYou are already connected to a server with "${bot.username}"\nYou can disconnect this session with the "[[;steelblue;]disconnect]" command.\n`)
     }
 
-    let opts = parseArgs(args);
+    let opts = parseArgs({ args,
+      strict: false,
+      options: {
+        username: {
+          type: 'string',
+          short: 'u'
+        },
+        auth: {
+          type: 'string',
+          short: 'a'
+        },
+        host: {
+          type: 'string',
+          short: 'h'
+        },
+        port: {
+          type: 'string',
+          short: 'p'
+        },
+        version: {
+          type: 'string',
+          short: 'v'
+        },
+        fakeHost: {
+          type: 'string'
+        }
+      }
+    }).values;
 
     // NOTE : When using shorthand (-h) only one `-` is needed, when using longhand (--host) two are needed.
-    let username = opts.u || opts.username;
-    if (!username) return console.log('You must specify a username with -u or --username')
-    let host = opts.h || opts.host;
-    if (!host) return console.log('You must specify a host with -h or --host')
-    let port = opts.p || opts.port;
-    if (!port) port = 25565;
-    let version = String(opts.v || opts.version);
-
-    console.log("Version Specified:", version)
-
-    if (!version) version = undefined;
-    console.log('Passed Version:', version)
-
-    // Temp Patch: 
-    if(version == '1.2') version = '1.20'
-
-    // if(version && !supportedVersions.includes(version) && !testedVersions.includes(version)) {
-    //   return console.log(`[[b;#999999;]Mine][[b;steelblue;]Prompt] » The version [[b;indianred;]${version}] is [[b;indianred;]unsupported], try a supported version.\n[[;steelblue;]Supported Versions:] ${supportedVersions.join(', ')}\n[[;steelblue;]Tested Versions:] ${testedVersions.join(', ')}`)
-    // }
-
-    let authentication = opts.a || opts.auth; // (Default: true)
-    if(typeof authentication !== 'boolean') console.log(`[[b;#999999;]Mine][[b;steelblue;]Prompt] » [[b;indianred;]Auth] should be passed as [[b;indianred;]true] or [[b;indianred;]false]`)
-
-    // TODO: Allow all flags passable to mineflayer
+    if (!opts.username) return console.log('You must specify a username with -u or --username')
+    if (!opts.host) return console.log('You must specify a host with -h or --host')
+    if (!opts.port) opts.port = 25565;
 
     let options = {
-      username: username,
-      host: host,
-      port: port,
-      version: version,
-      auth: (authentication === true) ? 'microsoft' : 'offline',
-      skipValidation: !authentication,
-      profilesFolder: path.join(minecraftFolderPath, 'mineprompt-cache', username.toUpperCase()),
-      fakeHost: opts.fakeHost || host, // Used on servers with TCPShield
+      username: opts.username,
+      host: opts.host,
+      port: opts.port,
+      version: opts.version,
+      auth: (opts.auth == 'true') ? 'microsoft' : 'offline',
+      skipValidation: !opts.auth,
+      profilesFolder: path.join(minecraftFolderPath, 'mineprompt-cache', opts.username.toUpperCase()),
+      fakeHost: opts.fakeHost || opts.host, // Used on servers with TCPShield
       onMsaCode: function(data) {
         console.log(`
           [[b;indianred;]Microsoft Authentication:]
@@ -75,15 +81,11 @@ module.exports = {
       logErrors: false
     }
 
-    // TODO: Switch to -e or -edition (Java, Bedrock)
-    let bedrock_edition = opts.b || opts.bedrock;
+    console.log(`[[b;#999999;]Mine][[b;steelblue;]Prompt] » Opening a connection to [[b;seagreen;]${opts.host}:${opts.port}]`)
+    console.debug(`[Connect] Player: ${opts.username}, Version: ${opts.version}, Auth: ${opts.auth}`)
 
-    console.log(`[[b;#999999;]Mine][[b;steelblue;]Prompt] » Opening a connection to [[b;seagreen;]${host}:${port}]`)
-    console.debug(`[Connect] Player: ${username}, Version: ${version}, Auth: ${authentication}`)
+    database.addConnection(opts.username, args.join(' '))
 
-    database.addConnection(args.join(' '))
-
-    if(bedrock_edition) return bedrock.startClient(options)
     return mineflayer.startClient(options);
 
   }
